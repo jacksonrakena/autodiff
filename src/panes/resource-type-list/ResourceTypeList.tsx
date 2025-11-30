43ae450e7df5e66e1a93af524cd7ff0257456ab9
jacksonrakena <jackson@rakena.co.nz>
2025-11-30 19:38:23 +1100

    add dubious quality search

--- src/panes/resource-type-list/ResourceTypeList.tsx
+++ src/panes/resource-type-list/ResourceTypeList.tsx
@@ -1,11 +1,14 @@
-import { Flex, ScrollArea, Box, Text } from "@radix-ui/themes";
+import { Flex, ScrollArea, Box, Text, TextField } from "@radix-ui/themes";
 import { invoke } from "@tauri-apps/api/core";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useRef } from "react";
 import { Link } from "react-router";
 import { useKubePathParams } from "../../util/kube/routes";
 import { makeKubePath } from "../../util/kube/routes";
 import { NoSelectStyle } from "../../util/platform";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
+import uFuzzy from "@leeoniya/ufuzzy";
+import { useKeyPress } from "../../util/keybinds";
 type ApiResource = {
   kind: string;
   plural: string;
@@ -18,8 +21,30 @@ type ApiGroup = {
   version: string;
   resources: ApiResource[];
 };
+const ufuzzy = new uFuzzy();
+
+export const executeSearch = (groups: ApiGroup[], query: string) => {
+  const flatList: { group: ApiGroup; resource: ApiResource }[] = [];
+  for (const g of groups) {
+    for (const r of g.resources) {
+      flatList.push({ group: g, resource: r });
+    }
+  }
+  const haystack = flatList.map((r) => r.resource.kind);
+  const filter = ufuzzy.filter(haystack, query);
+  if (!filter) return null;
+  const info = ufuzzy.info(filter, haystack, query);
+  const order = ufuzzy.sort(info, haystack, query);
+
+  return order.map((e) => {
+    const index = info.idx[e];
+    const resource = flatList[index];
+    return { resource, info: info.ranges[e] };
+  });
+};
 export const ResourceTypeList = () => {
   const kubeParams = useKubePathParams();
+  const [search, setSearch] = useState("");
   const [apiResources, setApiResources] = useState<ApiGroup[]>([]);
   useEffect(() => {
     (async () => {
@@ -41,67 +66,130 @@ export const ResourceTypeList = () => {
     return n;
   }, [apiResources]);
 
+  const searchedResources = useMemo(() => {
+    if (search.trim() === "") return filteredApiGroups;
+    const results = executeSearch(filteredApiGroups, search);
+    if (!results) return [];
+
+    return results.reduce<ApiGroup[]>((acc, curr) => {
+      let group = acc.find((g) => g.name === curr.resource.group.name);
+      if (!group) {
+        group = {
+          name: curr.resource.group.name,
+          version: curr.resource.group.version,
+          resources: [],
+        };
+        acc.push(group);
+      }
+      group.resources.push({ ...curr.resource.resource, ranges: curr.info });
+      return acc;
+    }, []);
+  }, [search, filteredApiGroups]);
+
   return (
-    <ScrollArea
-      scrollbars="vertical"
-      style={{
-        ...NoSelectStyle,
-      }}
-    >
-      <Flex
-        direction="column"
+    <>
+      <Box style={{ paddingLeft: "16px", paddingRight: "16px" }}>
+        <TextField.Root
+          value={search}
+          onChange={(e) => setSearch(e.currentTarget.value)}
+          size={"2"}
+          placeholder="Search resource types..."
+        >
+          <TextField.Slot>
+            <MagnifyingGlassIcon height="16" width="16" />
+          </TextField.Slot>
+        </TextField.Root>
+      </Box>
+      <ScrollArea
+        scrollbars="vertical"
         style={{
-          paddingLeft: "8px",
-          paddingRight: "16px",
-          fontSize: "13px",
+          ...NoSelectStyle,
         }}
-        gap="4"
       >
-        {filteredApiGroups.map((res) => (
-          <Box key={res.name}>
-            <Text
-              style={{
-                color: "var(--gray-11)",
-              }}
-            >
-              {res.name}
-            </Text>
-            <Flex direction={"column"}>
-              {res.resources.map((r) => {
-                const route =
-                  "/app" +
-                  makeKubePath({
-                    api_version: r.api_version,
-                    group: res.name,
-                    resource_plural: r.plural,
-                  });
-                return (
-                  <Link
-                    key={r.kind}
-                    style={{
-                      color: "black",
-                      textDecoration: "unset",
-                      backgroundColor:
-                        kubeParams.resource_plural === r.plural &&
-                        kubeParams.group === res.name
-                          ? "var(--gray-3)"
-                          : "transparent",
-                      paddingLeft: "4px",
-                      paddingTop: "2px",
-                      paddingBottom: "2px",
-                      borderRadius: "4px",
-                      //borderLeft: "1px solid var(--accent-8)",
-                    }}
-                    to={route}
-                  >
-                    {r.kind}
-                  </Link>
-                );
-              })}
-            </Flex>
-          </Box>
-        ))}
-      </Flex>
-    </ScrollArea>
+        <Flex
+          direction="column"
+          style={{
+            paddingLeft: "8px",
+            paddingRight: "16px",
+            fontSize: "13px",
+          }}
+          gap="4"
+        >
+          {searchedResources.map((res) => (
+            <Box key={res.name === "" ? "Core" : res.name}>
+              <Text
+                style={{
+                  color: "var(--gray-11)",
+                }}
+              >
+                {res.name === "" ? "Core" : res.name}
+              </Text>
+              <Flex direction={"column"}>
+                {res.resources.map((r) => {
+                  const route =
+                    "/app" +
+                    makeKubePath({
+                      api_version: r.api_version,
+                      group: res.name,
+                      resource_plural: r.plural,
+                    });
+                  return (
+                    <Link
+                      key={r.kind}
+                      style={{
+                        color: "black",
+                        textDecoration: "unset",
+                        backgroundColor:
+                          kubeParams.resource_plural === r.plural &&
+                          kubeParams.group === res.name
+                            ? "var(--gray-3)"
+                            : "transparent",
+                        paddingLeft: "4px",
+                        paddingTop: "2px",
+                        paddingBottom: "2px",
+                        borderRadius: "4px",
+                        //borderLeft: "1px solid var(--accent-8)",
+                      }}
+                      to={route}
+                    >
+                      {r.ranges ? (
+                        <BoldTextRange
+                          fullText={r.kind}
+                          startIndex={r.ranges[0]}
+                          endIndex={r.ranges[1]}
+                        />
+                      ) : (
+                        <>{r.kind}</>
+                      )}
+                    </Link>
+                  );
+                })}
+              </Flex>
+            </Box>
+          ))}
+        </Flex>
+      </ScrollArea>
+    </>
+  );
+};
+export const BoldTextRange = ({
+  fullText,
+  startIndex,
+  endIndex,
+}: {
+  fullText: string;
+  startIndex: number;
+  endIndex: number;
+}) => {
+  const beforeBold = fullText.substring(0, startIndex);
+  const boldPart = fullText.substring(startIndex, endIndex);
+  const afterBold = fullText.substring(endIndex);
+
+  return (
+    <span>
+      {beforeBold}
+      <span style={{ fontWeight: "bold" }}>{boldPart}</span>
+      {afterBold}
+    </span>
   );
 };
